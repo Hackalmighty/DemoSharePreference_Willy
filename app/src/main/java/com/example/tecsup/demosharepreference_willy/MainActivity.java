04fc9a85b8a7fe4162c79dc85653bbedefdc25fa
Hackalmighty <wbps85@gmail.com>
2019-05-13 19:07:09 -0500

    al inicio imprime willy, la segunda vez que lo ejecutas imprime santiago

--- app/src/main/java/com/example/tecsup/demosharepreference_willy/MainActivity.java
+++ app/src/main/java/com/example/tecsup/demosharepreference_willy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tecsup.demosharepreference_willy;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences pre = getApplicationContext().getSharedPreferences("demo_pref",MODE_PRIVATE);
+        SharedPreferences pref = getApplicationContext().
+                getSharedPreferences("demo_pref",MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = pref.edit();
+        String nombre = pref.getString("nombre", "Willy");
+
+        editor.putString("nombre", "Santiago");
+        editor.commit();
+        TextView t = findViewById(R.id.tv_nombre);
+        t.setText(nombre);
     }
 }
